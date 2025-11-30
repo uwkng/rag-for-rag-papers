1417c6b108dbf12ae3c2c0b506e8e4adb3557eb9
uwkng <koenig_u@gmx.net>
2025-11-30 01:04:23 +0100

    feat: [ingest.py] add data loader functionality for the PDFs

--- ingest.py
+++ ingest.py
@@ -1,1 +1,21 @@
 # Load & Chunk PDFs
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from pathlib import Path
+from pypdf import PdfReader
+
+DATA_PATH = Path("data/raw")
+
+def data_loader():
+    """
+    
+    """
+    all_docs = []
+
+    for pdf in DATA_PATH.iterdir():
+        reader = PdfReader(str(pdf))
+        text = ""
+        for page in reader.pages:
+            text += page.extract_text() or ""
+        all_docs.append(text)
+        return all_docs
