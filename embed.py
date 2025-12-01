147349949008d5188d15bce2de6d2c321639b060
uwkng <koenig_u@gmx.net>
2025-12-01 19:05:41 +0100

    feat: [embed.py] add embedding function

--- embed.py
+++ embed.py
@@ -1,1 +1,11 @@
-# Generate embeddings
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+
+embedding_model_name = "BAAI/bge-small-en"
+embedding_model_kwargs = {"device: cpu"}
+embedding_model_encode_kwargs = {"normalize_embeddings": True}
+
+embedding_model = HuggingFaceBgeEmbeddings(
+    model_name = embedding_model_name,
+    model_kwargs = embedding_model_kwargs,
+    encode_kwargs = embedding_model_encode_kwargs,
+)
