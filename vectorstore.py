926621008e319c137b39364a54637c5932c5b1f8
uwkng <koenig_u@gmx.net>
2025-12-05 22:40:52 +0100

    fix: [vectorstore.py] packed logic in a function

--- vectorstore.py
+++ vectorstore.py
@@ -1,14 +1,13 @@
 from langchain_chroma import Chroma
 
-from ingest import data_loader, chunking
-from embed import embedding_function
+def setup_vectorstore(all_docs, chunked_docs, embedding_func):
+    """
+    Sets up the vectorstore.
+    """
+    vector_store = Chroma.from_texts(
+        texts = chunked_docs,
+        embedding = embedding_func,
+        persist_directory="chroma_db"
+    )
 
-all_docs = data_loader()
-chunked_docs = chunking(all_docs)
-embedding_func = embedding_function()
-
-vector_store = Chroma.from_texts(
-    texts = chunked_docs,
-    embedding = embedding_func,
-    persist_directory="chroma_db"
-)
+    return vector_store
