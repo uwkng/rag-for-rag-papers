9091312c49b3e6f63b121047c494a1aae1782ab9
uwkng <koenig_u@gmx.net>
2025-12-05 19:17:14 +0100

    feat: [vectorstore.py] add vectorstore

--- vectorstore.py
+++ vectorstore.py
@@ -1,1 +1,18 @@
-# Initialize / store / query in Chroma
+from langchain_chroma import Chroma
+
+from ingest import data_loader, chunking
+from embed import embedding_function
+
+all_docs = data_loader()
+chunked_docs = chunking(all_docs)
+embedding_func = embedding_function()
+
+vector_store = Chroma(
+    chunked_docs,
+    embedding_function=embedding_func,
+    persist_directory="chroma_db"
+)
+
+results = vector_store.similarity_search(query="artifical intelligence", k=1)
+for doc in results:
+    print(f"* {doc.page_content} [{doc.metadata}]")
