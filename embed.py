0b0655dc75870aa7ae91da67ba09a1cd488f4256
uwkng <koenig_u@gmx.net>
2025-12-05 19:16:06 +0100

    fix: [embed.py] add embedding function

--- embed.py
+++ embed.py
@@ -1,11 +1,15 @@
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 
-embedding_model_name = "BAAI/bge-small-en"
-embedding_model_kwargs = {"device: cpu"}
-embedding_model_encode_kwargs = {"normalize_embeddings": True}
+def embedding_function():
 
-embedding_model = HuggingFaceBgeEmbeddings(
-    model_name = embedding_model_name,
-    model_kwargs = embedding_model_kwargs,
-    encode_kwargs = embedding_model_encode_kwargs,
-)
+    embedding_model_name = "BAAI/bge-small-en"
+    embedding_model_kwargs = {"device": "cpu"}
+    embedding_model_encode_kwargs = {"normalize_embeddings": True}
+
+    embedding_model = HuggingFaceBgeEmbeddings(
+        model_name = embedding_model_name,
+        model_kwargs = embedding_model_kwargs,
+        encode_kwargs = embedding_model_encode_kwargs,
+    )
+
+    return embedding_model
